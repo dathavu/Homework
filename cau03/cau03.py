6cd129d1ca16e2d5c682c4ffe1d169debdb79440
dathavu <havudatha@gmail.com>
2021-04-30 21:33:24 +0700

    file

---
+++ cau03/cau03.py
@@ -0,0 +1,9 @@
+#khai báo 2 biến dùng lệnh print để in ra màn hình giá trị sau khi khai báo
+#kiểu number
+a = int(input("khai báo a là số nguyên: "))
+print("số nguyên a: ",a)
+#kiểu chuỗi
+a = 'đạt '
+b = 'hà'
+c = a+b
+print("tên nick name:",str(c))
